5d5967012d26028ee1c6c8b90bb39a962781bb38
Marc Rousavy <marcrousavy@hotmail.com>
2019-07-23 15:51:08 +0200

    refactor to separate files

---
+++ js/scroll-reveal.js
@@ -0,0 +1,12 @@
+var optionsLeft = {
+    origin: 'left',
+    distance: '100%',
+    duration: 1000
+}
+var optionsRight = {
+    origin: 'right',
+    distance: '100%',
+    duration: 1000
+}
+ScrollReveal().reveal('.col-md-8', optionsLeft);
+ScrollReveal().reveal('.col-md-push-4', optionsRight);
